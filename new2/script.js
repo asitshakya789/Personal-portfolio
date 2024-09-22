2265752fbb056dd6f1163390ca2e9f3eddeb9b77
asitshakya678 <asitshakya789@gmail.com>
2024-09-22 23:34:23 +0530

    picture section

---
+++ new2/script.js
@@ -0,0 +1,9 @@
+let scrollImage = document.querySelector('.scroll-image');
+
+function scrollLeft() {
+    scrollImage.scrollLeft -= 350; // adjust the value to scroll by
+}
+
+function scrollRight() {
+    scrollImage.scrollLeft += 350; // adjust the value to scroll by
+}
